aa8b94557d2313fa3f61f15902c0cad436efa2fb
Nate Denys <nate_denys@yahoo.com>
2017-09-12 07:26:01 -0700

    edit routes to include a tags, allow adding new sites but not saved to a database yet, add body-parser library

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+		{name: "Belmont", image: "http://www.photosforclass.com/download/11407596925"},
+		{name: "Redwood", image: "http://www.photosforclass.com/download/2984976440"},
+		{name: "Mateo", image: "http://www.photosforclass.com/download/8265812638"}
+];
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Belmont", image: "http://www.photosforclass.com/download/11407596925"},
-		{name: "Redwood", image: "http://www.photosforclass.com/download/2984976440"},
-		{name: "Mateo", image: "http://www.photosforclass.com/download/8265812638"}
-	]
 	res.render('campgrounds', {campgrounds: campgrounds});
-})
+});
+
+app.post('/campgrounds', function(req, res){
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image}
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render('new.ejs');
+});
 
 app.listen(3000, function(){
 	console.log('working!');
