35339137366137ce0af273af6147d82140fc6053
Nate Denys <nate_denys@yahoo.com>
2017-09-12 23:28:52 -0700

    Intro Data Persistance, create MongoDB and Model Schemas

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require('mongoose')
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-		{name: "Belmont", image: "http://www.photosforclass.com/download/11407596925"},
-		{name: "Redwood", image: "http://www.photosforclass.com/download/2984976440"},
-		{name: "Mateo", image: "http://www.photosforclass.com/download/8265812638"},
-		{name: "Belmont", image: "http://www.photosforclass.com/download/11407596925"},
-		{name: "Redwood", image: "http://www.photosforclass.com/download/2984976440"},
-		{name: "Mateo", image: "http://www.photosforclass.com/download/8265812638"},
-		{name: "Belmont", image: "http://www.photosforclass.com/download/11407596925"},
-		{name: "Redwood", image: "http://www.photosforclass.com/download/2984976440"},
-		{name: "Mateo", image: "http://www.photosforclass.com/download/8265812638"}
-];
+
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Redwood", 
+// 		image: "http://www.photosforclass.com/download/2984976440"
+
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("Newly create campground:");
+// 			console.log(campground);
+// 		}
+// 	});
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render('campgrounds', {campgrounds: campgrounds});
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render('campgrounds', {campgrounds: allCampgrounds});
+		}
+	});
 });
 
 app.post('/campgrounds', function(req, res){
@@ -30,9 +50,15 @@ app.post('/campgrounds', function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image}
-	campgrounds.push(newCampground);
-	// redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	// cretae a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
 app.get("/campgrounds/new", function(req, res){
