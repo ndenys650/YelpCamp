bf2946510e9fb9dcf83afa742ed2e1d41edaa7f6
Nate Denys <nate_denys@yahoo.com>
2017-09-14 13:10:49 -0700

    successfully implement nested route for creating new comments
    add new comment button and cleared up route functionality to perform correctly

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express 	= require("express"),
-	app 				= express(),
+	app 		= express(),
 	bodyParser 	= require("body-parser"),
-	mongoose 		= require('mongoose'),
+	mongoose 	= require('mongoose'),
 	Campground 	= require("./models/campground"),
-	seedDB			= require("./seeds")
+	Comment 	= require("./models/comment"),
+	seedDB		= require("./seeds")
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -75,6 +76,30 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 			console.log(err);
 		} else {
 			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+// COMMENTS UPDATE Route
+app.post("/campgrounds/:id/comments", function(req, res){
+	// lookup campgrounds using ID
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// create new comment
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					// connect new comment to campground
+					campground.comments.push(comment);
+					campground.save();
+					// redirect
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
 		}
 	});
 });
