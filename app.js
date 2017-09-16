fde4678555f28e349deac07305a20a17b0586810
Nate Denys <nate_denys@yahoo.com>
2017-09-16 15:50:28 -0700

    add login logic

--- app.js
+++ app.js
@@ -148,6 +148,13 @@ app.get("/login", function(req, res){
 	res.render("login");
 });
 
+// handling login logic
+app.post("/login", passport.authenticate("local",
+	{
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}), function(req, res){
+});
 
 
 app.listen(3000, function(){
