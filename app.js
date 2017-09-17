29d13cb74102da0a9607b7b2815bf51541228fad
Nate Denys <nate_denys@yahoo.com>
2017-09-17 15:34:42 -0700

    Add method-override, create EDIT route, add edit button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express 	= require("express"),
 	mongoose 	= require('mongoose'),
 	passport	= require("passport"),
 	LocalStrategy = require("passport-local"),
+	methodOverride = require("method-override"),
 	Campground 	= require("./models/campground"),
 	Comment 	= require("./models/comment"),
 	User		= require("./models/user"),
@@ -20,6 +21,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB(); // seed the database
 
 
