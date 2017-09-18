64074f178c4f36bcd10ad0a8a37ba9d2b8ea9a6e
Nate Denys <nate_denys@yahoo.com>
2017-09-17 19:11:58 -0700

    Add Flash messages across site, add styling to homepage, created price editing ability

--- routes/comments.js
+++ routes/comments.js
@@ -38,6 +38,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 					campground.comments.push(comment);
 					campground.save();
 					// redirect
+					req.flash("success", "Successfully added comment");
 					res.redirect("/campgrounds/" + campground._id);
 				}
 			});
@@ -73,6 +74,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
 		if(err){
 			res.redirect("back");
 		} else {
+			req.flash("success", "Commented Deleted");
 			res.redirect("/campgrounds/" + req.params.id);
 		}
 	});
