cf6eaf079e0e9ef789cea1430314d5909f70326d
Nate Denys <nate_denys@yahoo.com>
2017-09-17 17:18:00 -0700

    add authorization for comments, hide or show buttons

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // EDIT Comments route
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err){
 			res.redirect("back");
@@ -56,7 +56,7 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 // UPDATE Comments route
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
 		if(err){
 			res.redirect("back");
@@ -67,7 +67,7 @@ router.put("/:comment_id", function(req, res){
 });
 
 // DESTROY comment route
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err){
 		if(err){
 			res.redirect("back");
@@ -83,6 +83,25 @@ function isLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+	if(req.isAuthenticated()){
+			Comment.findById(req.params.comment_id, function(err, foundComment){
+				if(err){
+					res.redirect("back");
+				} else {
+					// does user own the comment?
+					if(foundComment.author.id.equals(req.user._id)) {
+						next();
+					} else {
+						res.redirect("back");
+					}
+				}
+			});
+		} else {
+			res.redirect("back");
+		}
 }
 
 module.exports = router;
